175e46a3f935d0be443bee0a4599304279dd7af9
LobodPl <konta.lobod@o2.pl>
2020-12-03 19:03:21 +0100

    Added Logout Button

--- WebApplication2/Seeder/IdentitySeedData.cs
+++ WebApplication2/Seeder/IdentitySeedData.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApplication2.Seeder
@@ -9,15 +6,16 @@ namespace WebApplication2.Seeder
     public class IdentitySeedData
     {
         private const string adminUser = "Admin";
-        private const string adminPassword = "admin123";
+        private const string adminPassword = "Pa$$word1";
 
         public static async Task EnsurePopulated(UserManager<IdentityUser> userManager)
         {
-            IdentityUser user = await userManager.FindByIdAsync(adminUser);
+            IdentityUser user = await userManager.FindByNameAsync(adminUser);
             if (user == null)
             {
                 user = new IdentityUser("Admin");
-                await userManager.CreateAsync(user, adminPassword);
+                IdentityResult result = await userManager.CreateAsync(user, adminPassword);
+                System.Diagnostics.Debug.WriteLine(result);
             }
         }
     }
